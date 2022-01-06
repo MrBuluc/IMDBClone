5511ea50eb40d78f4ff3146a63508b45b95a5e53
U180709061 <hakkicanbuluc@gmail.com>
2022-01-07 00:11:36 +0300

    -added fromMap() to User -checked this movie or tv series is favorite or not in DetailActivity -did favorite this movie or tv series -added star icon to activity_detail

--- app/src/main/java/com/hakkicanbuluc/imdbclone/model/User.java
+++ app/src/main/java/com/hakkicanbuluc/imdbclone/model/User.java
@@ -2,14 +2,15 @@ package com.hakkicanbuluc.imdbclone.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class User {
-    String userID;
-    String name;
-    String lastname;
-    String email;
-    ArrayList<String> favoriteMovies;
-    ArrayList<String> favoriteTvSeries;
+    private String userID;
+    private String name;
+    private String lastname;
+    private String email;
+    private ArrayList<String> favoriteMovies;
+    private ArrayList<String> favoriteTvSeries;
 
     public User(String userID, String name, String lastname, String email) {
         this.userID = userID;
@@ -18,6 +19,22 @@ public class User {
         this.email = email;
         this.favoriteMovies = new ArrayList<>();
         this.favoriteTvSeries = new ArrayList<>();
+    }
+
+    public User(String userID, String name, String lastname, String email, ArrayList<String>
+            favoriteMovies, ArrayList<String> favoriteTvSeries) {
+        this.userID = userID;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.favoriteMovies = favoriteMovies;
+        this.favoriteTvSeries = favoriteTvSeries;
+    }
+
+    public static User fromMap(Map<String, Object> map) {
+        return new User(map.get("UserID").toString(), map.get("Name").toString(), map.get("Lastname")
+                .toString(), map.get("Email").toString(), (ArrayList<String>) map.
+                get("Favorite Movies"), (ArrayList<String>) map.get("Favorite Tv Series"));
     }
 
     public HashMap<String, Object> toMap() {
@@ -29,5 +46,25 @@ public class User {
         map.put("Favorite Movies", favoriteMovies);
         map.put("Favorite Tv Series", favoriteTvSeries);
         return  map;
+    }
+
+    public ArrayList<String> getFavoriteMovies() {
+        return favoriteMovies;
+    }
+
+    public ArrayList<String> getFavoriteTvSeries() {
+        return favoriteTvSeries;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID='" + userID + '\'' +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", favoriteMovies=" + favoriteMovies +
+                ", favoriteTvSeries=" + favoriteTvSeries +
+                '}';
     }
 }
--- app/src/main/java/com/hakkicanbuluc/imdbclone/view/DetailActivity.java
+++ app/src/main/java/com/hakkicanbuluc/imdbclone/view/DetailActivity.java
@@ -2,20 +2,37 @@ package com.hakkicanbuluc.imdbclone.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.hakkicanbuluc.imdbclone.R;
 import com.hakkicanbuluc.imdbclone.model.OpenModel;
+import com.hakkicanbuluc.imdbclone.model.User;
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
     OpenModel openModel;
     TextView txtTitle, txtReleased, txtRunTime, txtCategories, txtDirector,
             txtWriter, txtActors, txtSummary, txtRating, txtVotes, txtType;
-    ImageView imagePoster;
+    ImageView imagePoster, imageFavorite;
+    ConstraintLayout layout;
+    DocumentReference userDoc;
+    User user;
+    boolean isThereImdbID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +42,30 @@ public class DetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         openModel = (OpenModel) intent.getSerializableExtra("openModel");
 
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        userDoc = firestore.collection("Users").document(currentUser.getUid());
+        userDoc.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot documentSnapshot = task.getResult();
+                if (documentSnapshot.exists()) {
+                    user = User.fromMap(documentSnapshot.getData());
+                    if (openModel.getType().equals("series")) {
+                        isThereImdbID = search(1, openModel.getImdbID());
+                    } else
+                    isThereImdbID = search(0, openModel.getImdbID());
+                    if (isThereImdbID) imageFavorite.setImageResource(android.R.drawable.btn_star_big_on);
+                    layout.setVisibility(View.VISIBLE);
+                }
+            } else {
+                Log.d("UserDoc.get.onComplete.task.isNotSuccessful", task.getException().
+                        toString());
+            }
+        });
+
         bindingViews();
 
         setting();
-
-
 
     }
 
@@ -47,6 +83,9 @@ public class DetailActivity extends AppCompatActivity {
         txtType = findViewById(R.id.txtType);
 
         imagePoster = findViewById(R.id.imagePoster);
+        imageFavorite = findViewById(R.id.imageFavorite);
+
+        layout = findViewById(R.id.layout);
     }
 
     private void setting() {
@@ -64,11 +103,70 @@ public class DetailActivity extends AppCompatActivity {
         String type = "";
         if (openModel.getType().equals("series"))
             type += "Tv ";
+        else
         type += openModel.getType();
         txtType.setText(type);
 
         Picasso.get()
                 .load(openModel.getPosterUrl())
                 .into(imagePoster);
+
+        layout.setVisibility(View.INVISIBLE);
+    }
+
+    private boolean search(int type, String imdbID) {
+        if (type == 1) {
+            for (String tvSeriesID : user.getFavoriteTvSeries()) {
+                if (tvSeriesID.equals(imdbID)) return true;
+            }
+        } else {
+            for (String movieID : user.getFavoriteMovies()) {
+                if (movieID.equals(imdbID)) return true;
+            }
+        }
+        return false;
+    }
+
+    public void doFavorite(View view) {
+        doTask(openModel.getType().equals("movie"), !isThereImdbID);
+    }
+
+    public void doTask(boolean isMovie, boolean isAdd) {
+        Task task;
+        String message, addStr;
+        if (isAdd) {
+            addStr = "added";
+            if (isMovie) {
+                task = userDoc.update("Favorite Movies", FieldValue.arrayUnion(openModel
+                        .getImdbID()));
+                message = "Movie";
+            } else {
+                task = userDoc.update("Favorite Tv Series", FieldValue.arrayUnion(openModel.
+                        getImdbID()));
+                message = "Tv Series";
+            }
+        } else {
+            addStr = "removed";
+            if (isMovie) {
+                task = userDoc.update("Favorite Movies", FieldValue.arrayRemove(openModel
+                        .getImdbID()));
+                message = "Movie";
+            } else {
+                task = userDoc.update("Favorite Tv Series", FieldValue.arrayRemove(openModel
+                        .getImdbID()));
+                message = "Tv Series";
+            }
+        }
+
+        task.addOnSuccessListener((OnSuccessListener<Void>) unused -> {
+            Toast.makeText(
+                    DetailActivity.this, openModel.getTitle() + " " + addStr + " to your" +
+                            " favorite " + message + " list", Toast.LENGTH_LONG).show();
+            imageFavorite.setImageResource(android.R.drawable.btn_star_big_on);
+            isThereImdbID = !isThereImdbID;
+        }).addOnFailureListener(e ->
+                Toast.makeText(DetailActivity.this, openModel.getTitle() + " could not be " +
+                                addStr + " to your favorite " + message + " list\n" + e.getLocalizedMessage(),
+                        Toast.LENGTH_LONG).show());
     }
 }
